9403ac3271aa5f3d734584e809468da02935a03b
Carlos Barrera <carlostonatihu@gmail.com>
2019-02-03 17:36:47 -0600

    Musica listo

--- Ejemplos/src/ejemplos/Musica.java
+++ Ejemplos/src/ejemplos/Musica.java
@@ -7,18 +7,39 @@
 package ejemplos;
 
 import javax.microedition.midlet.*;
+import javax.microedition.lcdui.*;
+import javax.microedition.media.Manager;
+import javax.microedition.media.Player;
 
 /**
  * @author tona
  * Created on 1/02/2019
  */
 public class Musica extends MIDlet {
-    public void startApp() {
-    }
+   Player midiPlayer = null;
 
-    public void pauseApp() {
+  public Musica() {
+    try {
+      midiPlayer = Manager.createPlayer(getClass().getResourceAsStream("/resources/back.mid"), "audio/midi");
+      midiPlayer.setLoopCount(5);
+    } catch (Exception e) {
+      System.err.println(e);
     }
+  }
 
-    public void destroyApp(boolean unconditional) {
+  public void startApp() {
+    try {
+      if (midiPlayer != null) {
+        midiPlayer.start();
+      }
+    } catch (Exception e) {
+      System.err.println(e);
     }
+  }
+
+  public void pauseApp() {
+  }
+
+  public void destroyApp(boolean unconditional) {
+  }
 }
