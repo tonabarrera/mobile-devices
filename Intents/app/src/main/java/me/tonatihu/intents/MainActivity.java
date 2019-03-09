e6762ec1ada6d217648297df053159b71b073edd
Tonatihu <carlostonatihu@gmail.com>
2019-03-09 13:29:24 -0600

    Iniciando intents

---
+++ Intents/app/src/main/java/me/tonatihu/intents/MainActivity.java
@@ -0,0 +1,23 @@
+package me.tonatihu.intents;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickEjemplo(View view) {
+    }
+
+    public void onClickFormulario(View view) {
+    }
+
+    public void onClickEcuacion(View view) {
+    }
+}
