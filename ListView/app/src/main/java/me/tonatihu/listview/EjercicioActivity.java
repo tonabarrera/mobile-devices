f8a43d1c7a258158b4fd0372f566221952a6e234
Tonatihu <carlostonatihu@gmail.com>
2019-03-21 20:58:19 -0600

    Nada

---
+++ ListView/app/src/main/java/me/tonatihu/listview/EjercicioActivity.java
@@ -0,0 +1,13 @@
+package me.tonatihu.listview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EjercicioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ejercicio);
+    }
+}
---
+++ ListView/app/src/main/java/me/tonatihu/listview/ListaAdapter.java
@@ -0,0 +1,8 @@
+package me.tonatihu.listview;
+
+/**
+ * @author tonatihu
+ * Created on 3/21/19
+ */
+public class ListaAdapter {
+}
