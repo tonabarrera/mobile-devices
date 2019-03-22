00c091d15d96addc40fb937a2598424dfddbb5a2
Tonatihu <carlostonatihu@gmail.com>
2019-03-21 19:22:10 -0600

    Listas

---
+++ Listas/app/src/main/java/me/tonatihu/listas/EjemploUnoActivity.java
@@ -0,0 +1,13 @@
+package me.tonatihu.listas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EjemploUnoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ejemplo_uno);
+    }
+}
