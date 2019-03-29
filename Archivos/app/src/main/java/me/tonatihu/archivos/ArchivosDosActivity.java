a974a60385dafc8d44b1111f2a92a9b34f34163e
Tonatihu <carlostonatihu@gmail.com>
2019-03-28 21:38:59 -0600

    Nada

--- Archivos/app/src/main/java/me/tonatihu/archivos/ArchivosDosActivity.java
+++ Archivos/app/src/main/java/me/tonatihu/archivos/ArchivosDosActivity.java
@@ -1,8 +1,13 @@
 package me.tonatihu.archivos;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import java.io.File;
@@ -11,26 +16,43 @@ import java.io.FileOutputStream;
 import java.io.PrintWriter;
 
 public class ArchivosDosActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE_WRITE_EXTERNAL_STORAGE_PERMISSION = 1;
     TextView jtv;
-    File f, d, r;
+    File f, nuevaCarpeta, r;
     FileOutputStream fos;
     PrintWriter pw;
-    String s;
+    String estado;
+    private static final String TAG = ArchivosDosActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_archivos);
+        // Check whether this app has write external storage permission or not.
+        int writeExternalStoragePermission = ContextCompat
+                .checkSelfPermission(ArchivosDosActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        // If do not grant write external storage permission.
+        if(writeExternalStoragePermission!= PackageManager.PERMISSION_GRANTED) {
+            // Request user to grant write external storage permission.
+            ActivityCompat.requestPermissions(ArchivosDosActivity.this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE_WRITE_EXTERNAL_STORAGE_PERMISSION);
+        }
         jtv = findViewById(R.id.xtv);
-        s = Environment.getExternalStorageState();    // Tarjeta SD disponible.
-        jtv.append("\nEstado Actual: " + s);
+        estado = Environment.getExternalStorageState();    // Tarjeta SD disponible.
+        jtv.append("\nEstado Actual: " + estado);
         r = Environment.getExternalStorageDirectory();// Directorio en tarjeta SD.
-        d = new File(r.getAbsolutePath() + "/datos"); // Crea carpeta datos en SD.
-        d.mkdirs();
-        jtv.append("\n\nNueva Carpeta: " + d + "\n\nContenido de la Carpeta " + r + ":");
-        String[] lista = r.list();
+        nuevaCarpeta = new File(r, "dato"); // Crea carpeta datos en SD.
+        Log.d(TAG, "Permisos de escritura: " + r.canWrite() + ": Permisos de lectura: " + r.canRead());
+
+        if (nuevaCarpeta.mkdirs())
+            Log.d(TAG, "Si se creo");
+        else
+            Log.d(TAG, "No se creo");
+        jtv.append("\n\nNueva Carpeta: " + nuevaCarpeta + "\n\nContenido de la Carpeta " + r + ":");
+       String[] lista = r.list();
         for (String aLista : lista) jtv.append("\n" + aLista);
-        f = new File(d, "misdatos.txt");                  // Abre datos1.txt en carpeta datos.
+
+        f = new File(nuevaCarpeta, "misdatos.txt");                  // Abre datos1.txt en carpeta datos.
         try {
             fos = new FileOutputStream(f);
             pw = new PrintWriter(fos);
@@ -42,5 +64,6 @@ public class ArchivosDosActivity extends AppCompatActivity {
             fnfe.printStackTrace();
             jtv.append("\n\nEXCEPTION:\n" + fnfe);
         }
+
     }
 }
--- Archivos/app/src/main/java/me/tonatihu/archivos/MainActivity.java
+++ Archivos/app/src/main/java/me/tonatihu/archivos/MainActivity.java
@@ -1,7 +1,9 @@
 package me.tonatihu.archivos;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickUno(View view) {
+        startActivity(new Intent(this, ArchivosActivity.class));
+    }
+
+    public void onClickDos(View view) {
+        startActivity(new Intent(this, ArchivosDosActivity.class));
     }
 }
