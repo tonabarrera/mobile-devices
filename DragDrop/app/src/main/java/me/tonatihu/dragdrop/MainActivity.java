09a7c7cff744050e65a0b2594644926255cb4ba0
Tonatihu <carlostonatihu@gmail.com>
2019-05-23 22:58:43 -0500

    Drag and drop

--- DragDrop/app/src/main/java/me/tonatihu/dragdrop/MainActivity.java
+++ DragDrop/app/src/main/java/me/tonatihu/dragdrop/MainActivity.java
@@ -14,83 +14,84 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
-    ImageView ima;
+
     private static final String IMAGEVIEW_TAG = "Android Logo";
-    String msg;
+    private String msg;
+    private int x_cord = 0;
+    private int y_cord = 0;
+
+
+    ImageView ima;
+    private View mainView;
     private android.widget.RelativeLayout.LayoutParams layoutParams;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mainView = findViewById(R.id.container);
         ima = findViewById(R.id.iv_logo);
         // Sets the tag
         ima.setTag(IMAGEVIEW_TAG);
+
         ima.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
-                ClipData.Item item = new ClipData.Item((CharSequence) v.getTag());
+                ClipData.Item item = new ClipData.Item((CharSequence)v.getTag());
                 String[] mimeTypes = {ClipDescription.MIMETYPE_TEXT_PLAIN};
-                ClipData dragData = new ClipData(v.getTag().toString(),
-                        mimeTypes, item);
+                ClipData dragData = new ClipData(v.getTag().toString(), mimeTypes, item);
                 // Instantiates the drag shadow builder.
                 View.DragShadowBuilder myShadow = new View.DragShadowBuilder(ima);
-
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                    v.startDragAndDrop(dragData, // the data to be dragged
-                            myShadow, // the drag shadow builder
-                            v, // no need to use local data
-                            0 // flags (not currently used, set to 0)
-                    );
-                } else {
-                    v.startDrag(dragData, // the data to be dragged
-                            myShadow, // the drag shadow builder
-                            v, // no need to use local data
-                            0 // flags (not currently used, set to 0)
-                    );
-                }
+                // Starts the drag
+                v.startDrag(dragData, // the data to be dragged
+                        myShadow, // the drag shadow builder
+                        null, // no need to use local data
+                        0 // flags (not currently used, set to 0)
+                );
                 return true;
             }
         });
+
+
         // Create and set the drag event listener for the View
-        ima.setOnDragListener(new View.OnDragListener() {
+        ima.setOnDragListener( new View.OnDragListener(){
             @Override
-            public boolean onDrag(View view, DragEvent event) {
-                int action = event.getAction();
-                switch (action) {
+            public boolean onDrag(View v, DragEvent event){
+                switch(event.getAction())
+                {
                     case DragEvent.ACTION_DRAG_STARTED:
-                        layoutParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
-                        Log.d(msg, "Action is DragEvent.ACTION_DRAG_STARTED");
+                        layoutParams = (RelativeLayout.LayoutParams)
+                                v.getLayoutParams();
+                        Log.d(msg, "Action is DragEvent.ACTION_DRAG_STARTED"); // Do nothing
                         break;
                     case DragEvent.ACTION_DRAG_ENTERED:
                         Log.d(msg, "Action is DragEvent.ACTION_DRAG_ENTERED");
-                        int x_cord = (int) event.getX();
-                        int y_cord = (int) event.getY();
                         break;
-                    case DragEvent.ACTION_DRAG_EXITED:
+                    case DragEvent.ACTION_DRAG_EXITED :
                         Log.d(msg, "Action is DragEvent.ACTION_DRAG_EXITED");
-                        x_cord = (int) event.getX();
-                        y_cord = (int) event.getY();
-                        layoutParams.leftMargin = x_cord;
-                        layoutParams.topMargin = y_cord;
-                        view.setLayoutParams(layoutParams);
+                        //v.setLayoutParams(layoutParams);
                         break;
-                    case DragEvent.ACTION_DRAG_LOCATION:
-                        Log.d(msg, "Action is DragEvent.ACTION_DRAG_LOCATION");
+                    case DragEvent.ACTION_DRAG_LOCATION :
+                        //Log.d(msg, "Action is DragEvent.ACTION_DRAG_LOCATION");
                         x_cord = (int) event.getX();
                         y_cord = (int) event.getY();
+                        System.out.println(x_cord + ", "+y_cord );
                         break;
                     case DragEvent.ACTION_DRAG_ENDED:
-                        Log.d(msg, "Action is DragEvent.ACTION_DRAG_ENDED");
-                        view.setLayoutParams(layoutParams);
+                        Log.d(msg, "Action is DragEvent.ACTION_DRAG_ENDED"); // Do nothing
+                        x_cord = (int) event.getX();
+                        y_cord = (int) event.getY();
+                        System.out.println(x_cord + ", "+y_cord );
+                        layoutParams.leftMargin = x_cord;
+                        layoutParams.topMargin = y_cord;
+                        v.setLayoutParams(layoutParams);
+                        //mainView.setPaddingRelative( x_cord, y_cord ,0,0);
+                        System.out.println(layoutParams.leftMargin + ", "+layoutParams.topMargin );
                         break;
                     case DragEvent.ACTION_DROP:
-                        Log.d(msg, "Action is DragEvent.ACTION_DROP");
-                        view.setLayoutParams(layoutParams);
+                        Log.d(msg, "ACTION_DROP event"); // Do nothing
                         break;
-                    default:
-                        break;
+                    default: break;
                 }
                 return true;
             }
